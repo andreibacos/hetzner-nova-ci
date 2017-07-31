3d9b0752f24a868531549098246c57c132e773a3
Andrei Bacos <andreibacos@gmail.com>
2017-07-31 13:43:13 +0200

    add missing scripts

--- jobs/build_devstack.sh
+++ jobs/build_devstack.sh
@@ -20,7 +20,7 @@ run_ssh_cmd_with_retry ubuntu@$DEVSTACK_IP $DEVSTACK_SSH_KEY "echo win_user=$WIN
 # git prep
 scp -r -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" -i $DEVSTACK_SSH_KEY $basedir/clonemap.yaml ubuntu@$DEVSTACK_IP:/home/ubuntu/
 run_ssh_cmd_with_retry ubuntu@$DEVSTACK_IP $DEVSTACK_SSH_KEY "pip install zuul==2.5.2"
-run_ssh_cmd_with_retry ubuntu@$DEVSTACK_IP $DEVSTACK_SSH_KEY "zuul-cloner -m /home/ubuntu/clonemap.yaml -v git://git.openstack.org $ZUUL_PROJECT --zuul-branch $ZUUL_BRANCH --zuul-ref $ZUUL_REF --zuul-url $ZUUL_SITE/p --workspace /opt/stack"
+run_ssh_cmd_with_retry ubuntu@$DEVSTACK_IP $DEVSTACK_SSH_KEY "/home/ubuntu/.local/bin/zuul-cloner -m /home/ubuntu/clonemap.yaml -v git://git.openstack.org $ZUUL_PROJECT --zuul-branch $ZUUL_BRANCH --zuul-ref $ZUUL_REF --zuul-url $ZUUL_SITE/p --workspace /opt/stack"
 
 # run devstack
 run_ssh_cmd_with_retry ubuntu@$DEVSTACK_IP $DEVSTACK_SSH_KEY "source /home/ubuntu/keystonerc && /home/ubuntu/bin/run_devstack.sh $hyperv01 $ZUUL_CHANGE" 5
